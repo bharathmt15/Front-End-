c2c7b5bd32eb6a9829259e68e10ebe4ecf2e3494
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2024-10-26 10:04:53 +0530

    Added few more content

---
+++ src/Components/Getus.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Getus() {
+    return (
+        <div>
+            <>Get us page where you can contact us</>
+        </div>
+    );
+}
--- src/Components/Navcontent.js
+++ src/Components/Navcontent.js
@@ -1,8 +1,9 @@
 import React from "react";
+
 export default function Navcontent() {
     return (
         <>
-            <div className="typ">Hey this is a typing box area</div>
+            <div className="text">Travel With Ease</div>
         </>
     );
 }
