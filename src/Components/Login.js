ad3d169372a433af606cc019f9866e6dbc6ca820
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2024-11-07 00:04:11 +0530

    Added axios and sent the data to the backend

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,51 +1,86 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export default function Login() {
+    const [post, setPost] = useState({
+        name: "",
+        ph_no: "",
+        password: "",
+    });
+
+    const navigate = useNavigate();
+
+    const handleInput = (event) => {
+        setPost({...post, [event.target.name]: event.target.value});
+    };
+
+    const handleSubmit = (event2) => {
+        event2.preventDefault();
+        axios
+            .post("http://localhost:4000/api/test", post)
+            .then((res) => {
+                navigate("/"); // Redirect to home page
+            })
+            .catch((err) => {
+                console.log("Error submitting form:", err);
+            });
+    };
+
     return (
-        <>
-            <div className="login">
-                <form className="form">
-                    <div className="form-title">
-                        <span>Welcome Back to Your</span>
-                    </div>
-                    <div className="title-2">
-                        <span>SPACE</span>
-                    </div>
-                    <div className="input-container">
-                        <input
-                            className="input-mail"
-                            type="email"
-                            placeholder="Enter email"
-                        />
-                        <span> </span>
-                    </div>
+        <div className="login">
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="form-title">
+                    <span>Welcome to Your</span>
+                </div>
+                <div className="title-2">
+                    <span>SPACE</span>
+                </div>
 
-                    <section className="bg-stars">
-                        <span className="star"></span>
-                        <span className="star"></span>
-                        <span className="star"></span>
-                        <span className="star"></span>
-                    </section>
+                <div className="input-container">
+                    <input
+                        className="input-name"
+                        type="text"
+                        placeholder="Enter name"
+                        value={post.name}
+                        onChange={handleInput}
+                        name="name"
+                    />
+                </div>
 
-                    <div className="input-container">
-                        <input
-                            className="input-pwd"
-                            type="password"
-                            placeholder="Enter password"
-                        />
-                    </div>
-                    <button type="submit" className="submit">
-                        <span className="sign-text">Sign in</span>
-                    </button>
+                <div className="input-container">
+                    <input
+                        className="input-ph_no"
+                        type="text"
+                        placeholder="Enter phone number"
+                        value={post.ph_no}
+                        onChange={handleInput}
+                        name="ph_no"
+                    />
+                </div>
 
-                    <p className="signup-link">
-                        No account?
-                        <a href="/" className="up">
-                            Sign up!
-                        </a>
-                    </p>
-                </form>
-            </div>
-        </>
+                <div className="input-container">
+                    <input
+                        className="input-pwd"
+                        type="password"
+                        placeholder="Enter password"
+                        value={post.password}
+                        onChange={handleInput}
+                        name="password"
+                    />
+                </div>
+
+                <button type="submit" className="submit">
+                    <span className="sign-text">Sign up</span>
+                </button>
+
+                <p className="signup-link">
+                    <span>Already have an account?</span>
+                    <a className="up" href="/">
+                        Login!
+                    </a>
+                </p>
+            </form>
+        </div>
     );
 }
